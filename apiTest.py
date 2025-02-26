6583341cdac87db0436464714c0ee8136f4299e7
Carinalinda <catcatarina193@gmail.com>
2025-02-26 20:18:29 -0300

    ajustes

--- apiTest.py
+++ apiTest.py
@@ -1,13 +1,25 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 app = Flask(__name__)
 
 usuarios = [
-    'Jovanna'
-    'Kaua'
-    'Hina'
+    'Jovanna',
+    'Kaua',
+    'Tina',
+    'Tapioca',
+    'Leide Laura',
 ]
 
 @app.route('/users', methods=['GET'])
-def usuarios():
-    return jsonify({'message': 'oii, tudo bem?'})
+def pegar_usuarios():
+    return jsonify({'users': usuarios})
+# Rota para Ana Maria Braga
+@app.route('/user/<numero>', methods=['GET'])
+def pegar_usuario(numero):
+    # numero = 4
+    localNumber = int(numero)
+
+    return jsonify({'users': usuarios[localNumber]})
+
+if __name__ == '__main__':
+    app.run(port=3000)
